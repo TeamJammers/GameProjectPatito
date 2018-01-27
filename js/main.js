956b2794c5875bc5de8cf759f8b8439b79d7443c
jose <pachonjcl@gmail.com>
2018-01-27 14:25:18 -0400

    add movement to pepino

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload:
 function preload() {
     game.load.tilemap('desert', 'assets/desert.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/tmw_desert_spacing.png');
-    game.load.image('car', 'assets/pepino.png');
+    game.load.spritesheet('car', 'assets/dude.png', 32, 48);
     // game.load.image('car', 'assets/starstruck/dude.png');
 }
 
@@ -26,8 +26,12 @@ function create() {
 
     layer.resizeWorld();
 
-    sprite = game.add.sprite(450, 80, 'car');
-    sprite.anchor.setTo(0.5, 0.5);
+    sprite = game.add.sprite(32, 32, 'car');
+    // sprite.anchor.setTo(0.5, 0.5);
+
+    sprite.animations.add('left', [0, 1, 2, 3], 10, true);
+    sprite.animations.add('turn', [4], 20, true);
+    sprite.animations.add('right', [5, 6, 7, 8], 10, true);
 
     game.physics.enable(sprite);
 
@@ -52,18 +56,22 @@ function update() {
     sprite.body.velocity.x = 0;
     sprite.body.velocity.y = 0;
     if (cursors.left.isDown) {
-    	sprite.body.velocity.x -= 200;
+        sprite.animations.play('left');
+        sprite.body.velocity.x -= 200;
     }
     if (cursors.right.isDown) {
-      sprite.body.velocity.x += 200;
+        sprite.animations.play('right');
+        sprite.body.velocity.x += 200;
     }
 
-    if (cursors.up.isDown){
+    if (cursors.up.isDown) {
         sprite.body.velocity.y -= 200;
-		}
-		if (cursors.down.isDown){
-			sprite.body.velocity.y += 200;
-		}
+        sprite.animations.play('turn');
+    }
+    if (cursors.down.isDown) {
+        sprite.animations.play('turn');
+        sprite.body.velocity.y += 200;
+    }
 
 }
 
